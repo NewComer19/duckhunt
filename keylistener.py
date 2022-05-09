f8dbd4e4cd8d1fa36352281ce3a8ade1e6e102d3
Anton Kosse <galey16@gmail.com>
2022-05-09 16:49:21 +0300

    Fixed DD and UD time when a user stops typing

--- keylistener.py
+++ keylistener.py
@@ -2,6 +2,7 @@ import time
 import csv
 from pynput import keyboard 
 from pynput.keyboard import Key
+import statistics
 
 keyPresses=dict()
 keyReleases=dict()
@@ -9,11 +10,12 @@ downUpTime=dict()
 holdTime=list()
 downDownTime=list()
 upDownTime=list()
-columns_name = ["session","H.time", "DD.time", "UP.time"]
+columns_name = ["session","H.time", "DD.time", "UD.time", "H.avg", "DD.avg", "UD.avg", "H.med", "DD.med", "UD.med"]
 session_counter = 0
 time_last_key_pressed = None
 time_key_pressed = None
 time_key_released = None
+max_time_limit_between_presses = 15000
 
 def on_key_release(key): #what to do on key-release
     global time_key_released
@@ -23,7 +25,7 @@ def on_key_release(key): #what to do on key-release
         downUpTime[key] = time_taken
         holdTime.append(time_taken)
         print("H time for key: ", key, "is: ", time_taken, " miliseconds")
-    if key == Key.f4:
+    if key == Key.f9:
         return False #stop detecting more key-releases
     
 
@@ -31,33 +33,59 @@ def on_key_press(key): #what to do on key-press
     global time_key_pressed
     global time_last_key_pressed
 
-    if key == Key.enter:
+    if key == Key.f10:
         global session_counter
         session_counter += 1
         summarise()
 
     time_key_pressed = time_in_millis()
     if time_last_key_pressed is not None:
-        print("DD time", time_key_pressed - time_last_key_pressed, "miliseconds")
-        downDownTime.append(time_key_pressed - time_last_key_pressed)
+        dd_time =  time_key_pressed - time_last_key_pressed
+        if dd_time >= max_time_limit_between_presses:
+            pass
+        else:
+            print("DD time",dd_time, "miliseconds")
+            downDownTime.append(dd_time)
 
     keyPresses[key] = time_key_pressed
     if time_key_released is not None:
-        print("UD time: ", time_in_millis() - time_key_released, " miliseconds")
-        upDownTime.append(time_in_millis() - time_key_released)
+        ud_time = time_in_millis() - time_key_released
+        if ud_time >= max_time_limit_between_presses:
+            pass
+        else:
+            print("UD time: ", ud_time, " miliseconds")
+            upDownTime.append(ud_time)
 
-    if key == Key.f4:
+    if key == Key.f9:
         return False #stop detecting more key-presses
     time_last_key_pressed = time_key_pressed
 
+def average(lst: list) -> float:
+    return sum(lst) / len(lst)
+
 def summarise():
-    data_rows = zip([[session_counter], holdTime, downDownTime, upDownTime])
+    data_rows = zip([[session_counter],
+     holdTime, downDownTime, upDownTime, # raw data
+    [average(holdTime)], [average(downDownTime)], [average(upDownTime)], # average
+    [statistics.median(holdTime)], [statistics.median(downDownTime)], [statistics.median(upDownTime)]]) # median value
     with open('summary_session_' + str(session_counter) + '.csv', 'w') as f: 
         write = csv.writer(f) 
         write.writerow(columns_name) 
         for row in data_rows:
             for item in row:
                 write.writerow(item)
+    keyPresses.clear()
+    keyReleases.clear()
+    downUpTime.clear()
+    holdTime.clear()
+    downDownTime.clear()
+    upDownTime.clear()
+    global time_last_key_pressed
+    global time_key_pressed
+    global time_key_released
+    time_last_key_pressed = None
+    time_key_pressed = None
+    time_key_released = None      
 
 def time_in_millis() -> int:
     return round(time.time() * 1000)
